df21f2c71bcbf7af79b67e0ae819de54b6dc214b
air <airscods@gmail.com>
2023-01-08 13:27:10 +0000

    Create template for pages index.html and products.html

--- products/views.py
+++ products/views.py
@@ -5,14 +5,43 @@ from django.shortcuts import render
 
 
 def index(request):
+    context = {
+        'title': 'Store',
+        'is_promotion': True,
+    }
     return render(
         request,
-        'products/index.html'
+        'products/index.html',
+        context,
     )
 
 
 def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'name': 'Синяя куртка The North Face',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+        ]
+    }
     return render(
         request,
-        'products/products.html'
+        'products/products.html',
+        context,
     )

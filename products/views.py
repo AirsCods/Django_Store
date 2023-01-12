b0f788ffcd28dd25a235060ff4e950970e68f10f
air <airscods@gmail.com>
2023-01-12 12:13:59 +0000

    Create login and logout views class

--- products/views.py
+++ products/views.py
@@ -1,44 +1,32 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from products.models import ProductCategory, Products, Basket
-from users.models import User
+from common.views import TitleMixin
 
 
-# Create your views here.
+class IndexView(TitleMixin, TemplateView):
+    template_name = 'products/index.html'
+    title = 'Store'
 
 
-def index(request):
-    context = {
-        'title': 'Store',
-        'is_promotion': True,
-    }
-    return render(
-        request,
-        'products/index.html',
-        context,
-    )
+class ProductsListView(TitleMixin, ListView):
+    model = Products
+    template_name = 'products/products.html'
+    paginate_by = 3
+    title = 'Store - Каталог'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
 
-def products(request, category_id=None, page_number=1):
-    products= Products.objects.filter(category__id=category_id) if category_id else Products.objects.all()
-
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
-
-    context = {
-        'title': 'Store - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-    }
-
-    return render(
-        request,
-        'products/products.html',
-        context,
-    )
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['categories'] = ProductCategory.objects.all()
+        return context
 
 
 @login_required
